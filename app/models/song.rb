2980fd75ffe4f964f76f1f2eed3b23bced5086c2
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-01 18:55:49 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
-  has_attached_file :album_cover default_url: ':style/default.png', styles: { thumb: "100x100>", original: "400x306>" }
+  has_attached_file :album_cover, default_url: ':style/default.png', styles: { thumb: "100x100>", original: "400x306>" }
   def artist_name
     self.try(:artist).try(:name)
   end
